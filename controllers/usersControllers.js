ae3aa6a407eb48a4c22a0ee48355f45bf16ef7f4
Alexa01821 <sahunia2004@gmail.com>
2023-12-29 20:30:27 +0200

    logout, currentUser, updateAvatar

--- controllers/usersControllers.js
+++ controllers/usersControllers.js
@@ -1,14 +1,25 @@
 const User = require("../db/models/user.js");
 const gravatar = require("gravatar");
 const jwt = require("jsonwebtoken");
+const path = require("path");
+const fs = require("fs/promises");
+
 const { SECRET_KEY } = process.env;
+const avatarDir = path.join(
+  __dirname,
+  "../",
+  "public",
+  "photo"
+);
 
 const signup = async (req, res) => {
   const { name, email, password } = req.body;
   const userFind = await User.findOne({ email });
 
   if (userFind) {
-    res.status(409).json({ message: "User with this email registered" });
+    res
+      .status(409)
+      .json({ message: "User with this email registered" });
     return;
   }
   const photo = gravatar.url(email);
@@ -30,20 +41,26 @@ const signup = async (req, res) => {
   const token = jwt.sign(payload, SECRET_KEY);
   await User.findByIdAndUpdate(newUser._id, { token });
 
-  res.status(201).json({ token, user: { email, name, photo } });
+  res
+    .status(201)
+    .json({ token, user: { email, name, photo } });
 };
 
 const signin = async (req, res) => {
   const { email, password } = req.body;
   const user = await User.findOne({ email });
   if (!user) {
-    res.status(401).json({ message: "Email or password is invalid" });
+    res
+      .status(401)
+      .json({ message: "Email or password is invalid" });
     return;
   }
 
   const checkPassword = await user.checkPassword(password);
   if (!checkPassword) {
-    res.status(401).json({ message: "Email or password is invalid" });
+    res
+      .status(401)
+      .json({ message: "Email or password is invalid" });
     return;
   }
   const payload = {
@@ -52,10 +69,46 @@ const signin = async (req, res) => {
   const token = jwt.sign(payload, SECRET_KEY);
   await User.findByIdAndUpdate(user._id, { token });
 
-  res.json({ token, user: { email, name: user.name, photo: user.photo } });
+  res.json({
+    token,
+    user: { email, name: user.name, photo: user.photo },
+  });
+};
+
+const logout = async (req, res) => {
+  const { _id } = req.user;
+
+  await User.findByIdAndUpdate(_id, {
+    token: "",
+  });
+
+  res.sendStatus(204);
+};
+
+const getCurrent = (req, res) => {
+  const { email, name, photo } = req.user;
+
+  res.json({ email, name, photo });
+};
+
+const updatePhoto = async (req, res) => {
+  const { _id } = req.user;
+  const { path: tempUpload, originalname } = req.file;
+  const filename = `${_id}_${originalname}`;
+  const resultUpdate = path.join(avatarDir, filename);
+  await fs.rename(tempUpload, resultUpdate);
+
+  const avatarURL = path.join("avatars", filename);
+
+  await User.findByIdAndUpdate(_id, { avatarURL });
+
+  res.json({ avatarURL });
 };
 
 module.exports = {
   signup,
   signin,
+  logout,
+  getCurrent,
+  updatePhoto,
 };
--- routes/users.js
+++ routes/users.js
@@ -1,20 +1,42 @@
 const express = require("express");
-const { signup, signin } = require("../controllers/usersControllers");
+const {
+  signup,
+  signin,
+  logout,
+  getCurrent,
+  updatePhoto,
+} = require("../controllers/usersControllers");
 const {
   userSignupSchema,
   userSigninSchema,
 } = require("../schemas/usersSchema");
 const authenticate = require("../middlewares/authenticate");
 const validationBody = require("../decoration/validationBody");
+const upload = require("../middlewares/upload");
 
 const router = express.Router();
 
-router.post("/signup", validationBody(userSignupSchema), signup);
+router.post(
+  "/signup",
+  validationBody(userSignupSchema),
+  signup
+);
 
-router.post("/login", validationBody(userSigninSchema), signin);
+router.post(
+  "/login",
+  validationBody(userSigninSchema),
+  signin
+);
 
-router.post("/logout", authenticate);
+router.post("/logout", authenticate, logout);
 
-router.get("/current", authenticate);
+router.get("/current", authenticate, getCurrent);
+
+router.patch(
+  "/photo",
+  authenticate,
+  upload.single("avatar"),
+  updatePhoto
+);
 
 module.exports = router;
